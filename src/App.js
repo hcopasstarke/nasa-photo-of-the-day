c4ea37977df4702832144e8cba7a272a0eaab10e
Haili Copas-Starke <hcopasstarke@gmail.com>
2019-07-10 17:34:59 -0700

    initial app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	const [picture, setPicture] = useState([])
+
+	useEffect(() => {
+		console.log("return statement")
+		axios
+			.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+			.then(res => {
+				setPicture(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
+
+	return (
+		<div className='App'>
+			<h1>NASA's Awesome Photo Of The Day</h1>
+			<p>
+				<h2>{picture.title}</h2>
+
+				<img src={picture.url} alt='NASA Photo' />
+			</p>
+		</div>
+	)
 }
 
-export default App;
+export default App
